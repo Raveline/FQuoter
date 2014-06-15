956b8ed92ecc925a19df335e4f61eec9ff817ed7
Raveline <eraveline@gmail.com>
2014-06-15 23:18:09 +0200

    Added Eq deriving and the DatabaseValue type.

--- src/FQuoter/Quote.hs
+++ src/FQuoter/Quote.hs
@@ -12,22 +12,26 @@ type MetadataDictionary = Map.Map MetadataInfo MetadataValue
 
 anonymous = "Anonymous"
 
+data DatabaseValue a = DatabaseValue { primary_key :: Integer
+                                     , value :: a
+                                     }
+
 data Author = Author { first_name :: Maybe String
                      , last_name :: Maybe String
                      , surname :: Maybe String
-                     }
+                     } deriving (Eq)
 
 data Source = Source  { title :: String
                       , authors :: [Author]
                       , metadata :: MetadataDictionary
-                      }
+                      } deriving (Eq)
 
 data Quote = Quote { author :: Author
                    , content :: String
                    , location :: String
                    , tags :: [Tag]
                    , comment :: String
-                   }
+                   } deriving (Eq)
 
 instance Show Author where
     show (Author (Just f) (Just l) Nothing) = f ++ " " ++ l
