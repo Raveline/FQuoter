1df318a2cf3b87e60280aaf64fa6a7c232ad1622
Raveline <eraveline@gmail.com>
2014-06-16 18:20:35 +0200

    Bug steming from stupid sqlite non-typeness fixed.

--- src/FQuoter/Serialize.hs
+++ src/FQuoter/Serialize.hs
@@ -9,6 +9,7 @@ where
 
 import Data.List.Split
 
+import Data.Maybe
 import Database.HDBC
 import Database.HDBC.Sqlite3
 
@@ -17,8 +18,8 @@ import FQuoter.Quote
 type DBAuthor = DatabaseValue Author
 
 insertAuthor = "INSERT INTO Author VALUES (?,?,?,?)"
-findAuthor = "SELECT FROM Author\
-            \WHERE first_name || \" \" || last_name like ?\
+findAuthor = "SELECT * FROM Author \
+            \WHERE first_name || \" \" || last_name like ? \
             \OR surname like ?"
 updateAuthor = "UPDATE author SET first_name = ?, last_name = ?, surname = ? WHERE id_author = ?"
 deleteAuthor = "DELETE FROM Author id_author = ?"
@@ -30,9 +31,12 @@ serializeAuthor conn = run conn insertAuthor . authorToSqlValues
 -- Look up an author by it's name or it's surname
 lookUpAuthor :: (IConnection c) => c -> String -> IO [DBAuthor]
 lookUpAuthor conn search = do
-                        result <- quickQuery' conn insertAuthor 
-                                    [toSql ("%" ++ search ++ "%")]
+                        result <- quickQuery' conn findAuthor
+                            $ searchArray search
                         return $ map sqlValuesToAuthor result
+
+searchArray :: String -> [SqlValue]
+searchArray search = replicate 2 $ toSql $ "%" ++ search ++ "%"
 
 -- Convert an author to a list of SqlValue for insertion
 authorToSqlValues :: Author -> [SqlValue]
@@ -56,18 +60,23 @@ maybeStringToSql Nothing        = toSql ""
 maybeStringToSql (Just s)       = toSql s
 
 sqlToMaybeString :: SqlValue -> Maybe String
-sqlToMaybeString (SqlString []) = Nothing
-sqlToMaybeString (SqlString s)  = Just s
-sqlToMaybeString _              = Nothing
+sqlToMaybeString s@(SqlByteString _)  = stringToMaybe $ fromSql s 
+    where
+        stringToMaybe :: String -> Maybe String
+        stringToMaybe [] = Nothing
+        stringToMaybe s = Just s
+sqlToMaybeString _ = Nothing
 
 -- Create a new database from a schema.sql file
-buildNewDB :: FilePath  -- DB to create
-           -> FilePath  -- Schema file
+buildNewDB :: FilePath  -- Schema file
+           -> FilePath  -- DB to create
            -> IO ()
-buildNewDB toCreate schemaF = do
+buildNewDB schemaF toCreate = do
                                 conn <- connectSqlite3 toCreate
                                 commands <- readSchema schemaF
                                 mapM (flip (run conn) []) . init $ commands
+                                commit conn
+                                disconnect conn
                                 return ()
 
 readSchema :: FilePath -> IO [String]
