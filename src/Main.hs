0512463a90b0c35efc9bf3d95ef90ac940ba8f1b
Raveline <eraveline@gmail.com>
2014-08-01 13:24:34 +0000

    Added default folder & config.

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,7 @@
 import System.Environment
 import Database.HDBC
-import Database.HDBC.Sqlite3
 import Control.Monad.Except
 import Control.Monad.Reader
-import System.Environment.XDG.BaseDir (getUserDataDir, getUserDataFile)
 
 import FQuoter.Parser.ParserTypes
 import FQuoter.Parser.Parser
@@ -11,29 +9,30 @@ import FQuoter.Serialize.Serialize
 import FQuoter.Serialize.Shortcuts
 import FQuoter.Serialize.SerializedTypes
 import FQuoter.Serialize.Serialize
+import FQuoter.Config.Config
 
-configFolder = getUserDataDir "fquoter"
 
 main :: IO ()
 main = do
-        args <- getArgs 
+        args <- getArgs
+        config <- readConfig 
         case parseInput(unwords args) of 
             Left s -> print s
-            Right c -> executeCommand c
+            Right c -> executeCommand config c
 
-executeCommand :: Action -> IO ()
-executeCommand (Insert x) = insertAndDisplay x
-executeCommand (FindWord w) = do db <- getDB
-                                 result <- runExceptT $ runReaderT (process (searchWord w)) db
-                                 case result of
-                                    Left _ -> error "Should not happen. I think ?"
-                                    Right qs -> displayQuotes qs
-executeCommand (FindTags ts) = do db <- getDB
-                                  result <- runExceptT $ runReaderT (process (searchTags ts)) db
-                                  case result of
-                                    Left _ -> error "Should not happen. I think ?"
-                                    Right qs -> displayQuotes qs
-executeCommand _ = putStrLn "Not implemented yet."
+executeCommand :: Config -> Action -> IO ()
+executeCommand c (Insert x) = insertAndDisplay c x
+executeCommand c (FindWord w) = do db <- accessDB c
+                                   result <- runExceptT $ runReaderT (process (searchWord w)) db
+                                   case result of
+                                        Left _ -> error "Should not happen. I think ?"
+                                        Right qs -> displayQuotes qs
+executeCommand c (FindTags ts) = do db <- accessDB c
+                                    result <- runExceptT $ runReaderT (process (searchTags ts)) db
+                                    case result of
+                                        Left _ -> error "Should not happen. I think ?"
+                                        Right qs -> displayQuotes qs
+executeCommand _ _ = putStrLn "Not implemented yet."
 
 displayQuotes :: [SerializedType] -> IO ()
 displayQuotes = mapM_ displayQuote
@@ -42,19 +41,14 @@ displayQuotes = mapM_ displayQuote
         displayQuote (SQuote q) = print q
         displayQuotes _ = error "Not a quote. This should not happen !"
 
-insertAndDisplay :: ParsedType -> IO ()
-insertAndDisplay a = do db <- getDB 
-                        result <- runExceptT $ runReaderT (process (insert a)) db
-                        case result of
-                            Right _ -> do 
-                                        runReaderT (process commitAction) db
-                                        putStrLn $ "Inserted " ++ (show a)
-                            Left e -> print e >> runReaderT (process rollbackAction) db
+insertAndDisplay :: Config -> ParsedType -> IO ()
+insertAndDisplay c a = do db <- accessDB c
+                          result <- runExceptT $ runReaderT (process (insert a)) db
+                          case result of
+                                Right _ -> do runReaderT (process commitAction) db
+                                              putStrLn $ "Inserted " ++ (show a)
+                                Left e -> print e >> runReaderT (process rollbackAction) db
 
 
 handleError :: DBError -> IO ()
 handleError = putStrLn . show
-
--- Obviously temporary
-getDB :: IO Connection
-getDB = connectSqlite3 "test.db"
