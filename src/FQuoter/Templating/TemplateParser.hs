c49a156d014186f3cee8045b4253487e1afe1f88
Raveline <eraveline@gmail.com>
2014-08-08 12:34:42 +0000

    First version of the template parser.

---
+++ src/FQuoter/Templating/TemplateParser.hs
@@ -0,0 +1,49 @@
+module FQuoter.Templating.TemplateParser
+(parseTemplate)
+where
+
+import Text.ParserCombinators.Parsec
+import Control.Applicative hiding ((<|>), many)
+import Control.Monad
+import FQuoter.Templating.TemplateTypes
+
+parseTemplate = parse (many parseToken) ""
+
+parseToken :: GenParser Char st TokenNode
+parseToken = parseOr <|> parseOne <|> parseMany
+
+parseOr :: GenParser Char st TokenNode
+parseOr = char '|' *> (Or <$> tillPipe <*> tillPipe)
+    where tillPipe = parseToken `manyTill` (char '|')
+
+parseOne :: GenParser Char st TokenNode
+parseOne = One <$> parseMods <*> ( (char '%' *> parseContent) <|> parseConstant)
+
+parseMods :: GenParser Char st [TokenModificator]
+parseMods = option [] bracketContent 
+    where 
+        bracketContent :: GenParser Char st [TokenModificator]
+        bracketContent = between (char '{') (char '}') (parseMod `sepBy` (char ','))
+
+parseMany :: GenParser Char st TokenNode
+parseMany =  SomeAuthors <$> (return All) <*> parseTokens
+    where
+        parseTokens :: GenParser Char st [TokenNode]
+        parseTokens = between (char '[') (char ']') (many parseToken)
+
+parseContent :: GenParser Char st TokenContent
+parseContent = choice [try $ AuthorInfo <$> (string "af" *> return AuthorFirstName)
+                     ,try $ AuthorInfo <$> (string "al" *> return AuthorLastName)
+                     ,try $ AuthorInfo <$> (string "an" *> return AuthorNickName)
+                     ,SourceInfo <$> (string "t" *> return SourceTitle)
+                     ,SourceInfo <$> (SourceMetadata <$> (string "meta" *> many letter))]
+
+parseMod :: GenParser Char st TokenModificator
+parseMod = choice [string "cap" *> return Capital
+                  ,try $ string "init" *> return Initial
+                  ,string "it" *> return Italics]
+
+keyChar = "%{}[]|"
+
+parseConstant :: GenParser Char st TokenContent
+parseConstant = ConstantString <$> many1 (noneOf keyChar)
---
+++ testsuite/FQuoter/Templating/TemplateParserSpec.hs
@@ -0,0 +1,59 @@
+module FQuoter.Templating.TemplateParserSpec (main, spec) where
+
+import Text.ParserCombinators.Parsec.Error
+import Test.Hspec
+
+import FQuoter.Templating.TemplateTypes
+import FQuoter.Templating.TemplateParser
+
+main :: IO()
+main = hspec spec
+
+parse f = case parseTemplate f of
+            Left e -> error $ (show e)
+            Right x -> x
+
+simpleOne = "%al"
+simpleOne' = [One [] $ AuthorInfo AuthorLastName]
+simpleModOne = "{cap}%t"
+simpleModOne' = [One [Capital] $ SourceInfo SourceTitle]
+simpleOr = "|%al|%an|"
+simpleOr' = [Or [One [] $ AuthorInfo AuthorLastName] [One [] $ AuthorInfo AuthorNickName]]
+simpleMany = "[%al]"
+simpleMany' = [SomeAuthors All [One [] $ AuthorInfo AuthorLastName]]
+simpleConstant = "constant!"
+simpleConstant' = [One [] $ ConstantString simpleConstant]
+
+defaultTemplate = "[|{cap}%al, {init,cap}%af|{cap}%an|]"
+    ++ " (%metaDate) {it}%t. %metaPlace : %metaPublisher."
+
+defaultTree =   [SomeAuthors All
+                    [Or [One [Capital] $ AuthorInfo AuthorLastName
+                        ,One [] (ConstantString ", ")
+                        ,One [Initial, Capital] $ AuthorInfo AuthorFirstName]
+                        [One [Capital] $ AuthorInfo AuthorNickName]]
+                , One [] $ ConstantString " ("
+                 ,One [] (SourceInfo $ SourceMetadata "Date")
+                 ,One [] $ ConstantString ") "
+                 ,One [Italics] $ (SourceInfo SourceTitle)
+                 ,One [] $ ConstantString ". "
+                 ,One [] (SourceInfo $ SourceMetadata "Place")
+                 ,One [] $ ConstantString " : "
+                 ,One [] (SourceInfo $ SourceMetadata "Publisher")
+                 ,One [] $ ConstantString "."]
+
+spec = do
+    describe "Template strings are transformed in Template Tree." $ do
+        it "Read a simple template string with just one thing to display." $ do
+            parse simpleOne `shouldBe` simpleOne'
+        it "Read a template with just one thing to display, with modificator" $ do
+            parse simpleModOne `shouldBe` simpleModOne'
+        it "Read an Or case, with one thing to display or the other." $ do
+            parse simpleOr `shouldBe` simpleOr'
+        it "Read a multi-author display." $ do
+            parse simpleMany `shouldBe` simpleMany'
+        it "Read a constant." $ do
+            parse simpleConstant `shouldBe` simpleConstant'
+    describe "Default case can be handled." $ do
+        it "Display gives the default parsing tree." $ do
+            parse defaultTemplate `shouldBe` defaultTree
